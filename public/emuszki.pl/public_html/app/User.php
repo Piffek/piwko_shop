13deb9146ae0e5e39384bdb7124c674f983f1bfe
Piffek <patrykpiwko123412@gmail.com>
2017-01-11 21:39:32 +0100

    added

---
+++ public/emuszki.pl/public_html/app/User.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    public function product()
+    {
+    	return $this->hasOne('App\Items');
+    }
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password', 'surname', 'street', 'city', 'phone', 'companyname', 'nip','activated','token,'
+    ];
+
+    protected $guarded = ['_token'];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    //dzięki tej metodzie, eloquent będzie wiedział, jak szukać ról i jak dodać do roli
+    public function roles()
+    {
+    	return $this->belongsToMany(Roles::class, 'roles_has_users','users_id', 'roles_id')
+    	->withTimestamps();
+    }
+    
+    //sprawdza czy jest to tablica czy string
+    public function hasAnyRole($roles)
+    {
+    	if(is_array($roles))
+    	{
+    		foreach($roles as $role)
+    		{
+    			if($this->hasRole($role))
+    			{
+    				return true;
+    			}
+    		}
+    	}else if($this->hasRole($roles))
+    	{
+    		return true;
+    	}
+    	return false;
+    }
+    
+    //sprawdza na obiekcie roles(), jaką ma user ma role
+    public function hasRole($role)
+    {
+    	if($this->roles()->where('name', $role)->first())
+    	{
+    		return true;
+    	}
+    	return false;
+    }
+    
+    
+    public static function boot()
+    {
+    	parent::boot();
+    	
+    	static::creating(function($user)
+    	{
+    		$user->token = str_random(40);
+    	});
+    }
+    
+    public function hasVerified()
+    {
+    	$this->activated = true;
+    	$this->token=0;
+    	
+    	$this->save();
+    }
+    
+    
+    
+    
+    
+    
+    
+}
