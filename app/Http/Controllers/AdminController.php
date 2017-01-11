573266c061be3c7f036466abf97b2d3aed3013c2
Piffek <patrykpiwko123412@gmail.com>
2017-01-11 20:51:54 +0100

    add show new user function in admin panel

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,13 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Items;
 use Session;
+use App\User;
+use Carbon\Carbon;
 
 class AdminController extends Controller
 {
 	
-	public function index()
+	public function newUser()
 	{
-		return view('admin.index');
+		$current = Carbon::now();
+		$current = new Carbon();
+		
+		$today = $current->toDateString();
+		$new_user = User::where('created_at', $today)->count();  
+		return view('admin.index', compact('new_user'));
 	}
 
 
