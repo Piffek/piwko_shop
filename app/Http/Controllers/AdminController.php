c655727ac392abfc2342d9cea7bc843954c29ec9
Piffek <patrykpiwko123412@gmail.com>
2017-01-12 18:32:45 +0100

    change method view new order

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,17 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Items;
-use Session;
+
 use App\User;
+use App\Buyings;
+use App\LogOnData;
 use Carbon\Carbon;
+use View;
 
 class AdminController extends Controller
 {
 	
 	public function index()
 	{
-		return view('admin.index');
+		$current = Carbon::now();
+		$current = new Carbon();
+		 
+
+			
+		$today = $current->toDateString();
+		$new_user = User::where('created_at', $today)->count();
+			
+		$new_order = Buyings::where('created_at',$today)->count();
+			
+		$new_order_from_singoff = LogOnData::where('created_at',$today)->count();
+		//return view('admin.index',compact('new_user'),compact('new_order'),compact('new_order_from_singoff'));
+		return View::make('admin.index')
+		
+		->with(compact('new_user'))
+		
+		->with(compact('new_order'))
+		
+		->with(compact('new_order_from_singoff'));
 	}
 	
 	public function ViewNewUser()
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,10 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Kinds;
-use Carbon\Carbon;
-use App\User;
-use App\Buyings;
-use App\LogOnData;
+
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,25 +16,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-    	$current = Carbon::now();
-    	$current = new Carbon();
     	
         $kinds = Kinds::all();
 		 view()->share('kinds',$kinds);
 		 
-		 
-		 $today = $current->toDateString();
-		 $new_user = User::where('created_at', $today)->count();
-		 view()->share('new_user',$new_user);
-		 
-		 $new_order = Buyings::where('created_at',$today)->count();
-		 view()->share('new_order',$new_order);
-		 
-		 $new_order_from_singoff = LogOnData::where('created_at',$today)->count();
-		 view()->share('new_order_from_singoff', $new_order_from_singoff);
-		 
-		 
-		 
+ 
 		 
     }
 
