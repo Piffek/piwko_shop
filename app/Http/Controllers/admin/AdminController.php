5917d34ec3f92fa4aae404074d7abf3582b0776b
Piffek <patrykpiwko123412@gmail.com>
2017-01-24 18:45:41 +0100

    insert count sold products to chart

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Items;
 use Session;
+use DB;
+
 
 class AdminController extends Controller
 {
@@ -13,6 +15,28 @@ class AdminController extends Controller
 	public function index()
 	{
 		return view('admin.index');
+	}
+	
+	public function productChart()
+	{
+		//DB::table('booking')->selectRaw('count(id) as bookings_count, dayofweek(created_at) as weekday')->groupBy('weekday')->get();
+		//return view('admin.chart.productsChart');
+		return DB::table('buyings')->selectRaw('count(id) as buyings_count, dayofweek(created_at) as weekday')->groupBy('weekday')->get();
+	}
+	
+	public function soldChart()
+	{
+		return view('admin.chart.soldChart');
+	}
+	
+	public function deliveryChart()
+	{
+		return view('admin.chart.deliveryChart');
+	}
+	
+	public function paidChart()
+	{
+		return view('admin.chart.paidChart');
 	}
 
 
