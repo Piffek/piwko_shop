396c2b9579c1f57460226c60e5aa06343aa5e16b
Piffek <patrykpiwko123412@gmail.com>
2017-01-25 21:06:26 +0100

    add question to database

--- resources/views/admin/chart/deliveryChart.blade.php
+++ resources/views/admin/chart/deliveryChart.blade.php
@@ -5,25 +5,15 @@
   <div  id="myfirstchart" style="height: 250px; width: 500px;"></div>
   </div>
 <script>
-new Morris.Line({
-	  // ID of the element in which to draw the chart.
-	  parseTime:false,
+new Morris.Donut({
 	  element: 'myfirstchart',
-	  // Chart data records -- each entry in this array corresponds to a point on
-	  // the chart.
-	  
 	  data: [
-	    { day: 'DPD', value: {{ $delivery['0']->buyings_count}} },
-	    { day: 'UPS', value: {{ $delivery['1']->buyings_count}} },
+	    {label: "DPD", value: {{ $delivery['0']->buyings_count}}},
+	    {label: "UPS", value: {{ $delivery['1']->buyings_count}}},
 
-	  ],
-	  // The name of the data record attribute that contains x-values.
-	  xkey: 'day',
-	  // A list of names of data record attributes that contain y-values.
-	  ykeys: ['value'],
-	  // Labels for the ykeys -- will be displayed when you hover over the
-	  // chart.
-	  labels: ['Value']
+	  ]
 	});
+	
+
 </script>
 @endsection
