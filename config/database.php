04214c43d172d970de48992af3fece77fb85fc01
Piffek <patrykpiwko123412@gmail.com>
2017-02-19 20:21:50 +0100

    change name with pl on eng

--- config/database.php
+++ config/database.php
@@ -57,11 +57,11 @@ return [
     		
     		'mysql' => [
     				'driver' => 'mysql',
-    				'host' => env('DB_HOST', 'localhost'),
-    				'port' => env('DB_PORT', '3306'),
-    				'database' => env('DB_DATABASE', 'forge'),
-    				'username' => env('DB_USERNAME', 'forge'),
-    				'password' => env('DB_PASSWORD', ''),
+    				'host' => '127.0.0.1',
+    				'port' => '3306',
+    				'database' => 'users',
+    				'username' => 'root',
+    				'password' => '',
     				'charset' => 'utf8',
     				'collation' => 'utf8_unicode_ci',
     				'prefix' => '',
