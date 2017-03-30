28da0d662c1692b930ae0ecf3c01616bd827a1a1
Patryk Piwko <patrykpiwko123412@gmail.com>
2017-03-30 11:21:02 +0200

    refractor

--- resources/views/admin/roles/add.blade.php
+++ resources/views/admin/roles/add.blade.php
@@ -3,40 +3,39 @@
 
 <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg">Edytuj role</button>
 <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
-  <div class="modal-dialog modal-lg" role="document">
-    <div class="modal-content">
-    	<div class="panel panel-info">
-			<div class="panel-heading">
-    @foreach($roles as $role)
-     <form method="post" action="/admin/edit_role/{{$role->id}}">
-    	{!! csrf_field() !!}
-      <p>Nazwa: <input style="width:150px" id="role_name" type="text" class="form-control" name="name" value="{{ $role ->name }}">
-          <input type="submit" class="btn btn-success" value="Zapisz">
-     </form>
-          <a href="/admin/delete_role/{{$role->id}}" class="btn btn-danger">Usuń</a>
-     @endforeach
-     </form>
-     </div>
-     </div>
-    </div>
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="panel panel-info">
+				<div class="panel-heading">
+				    @foreach($roles as $role)
+				     <form method="post" action="/admin/edit_role/{{$role->id}}">
+					{!! csrf_field() !!}
+				      <p>Nazwa: <input style="width:150px" id="role_name" type="text" class="form-control" name="name" value="{{ $role ->name }}">
+					  <input type="submit" class="btn btn-success" value="Zapisz">
+				     </form>
+					  <a href="/admin/delete_role/{{$role->id}}" class="btn btn-danger">Usuń</a>
+				     @endforeach
+     				</form>
+			</div>
+		</div>
+	</div>
   </div>
 </div>
 
 
 
 <div class="row">
-<div class ="col-md-3 col-md-offset-4">
-	<div class="panel panel-info">
-		<div class="panel-heading"> 
-			<h3 class="panel-title">Dodaj nowa role</h3> </div>
-	      <form method="post" action="/admin/add_role">
-	      {!! csrf_field() !!}
-	       	<p>Nazwa: <input style="width:150px" id="role_name" type="text" class="form-control" name="name">
-			<br>
-			<input type="submit" class="btn btn-success" value="Zapisz"></tr>
-      </div>
-    </div>
-  </div>
+	<div class ="col-md-3 col-md-offset-4">
+		<div class="panel panel-info">
+			<div class="panel-heading"> 
+				<h3 class="panel-title">Dodaj nowa role</h3> </div>
+				      	<form method="post" action="/admin/add_role">
+				      	{!! csrf_field() !!}
+					<p>Nazwa: <input style="width:150px" id="role_name" type="text" class="form-control" name="name"><br>
+					<input type="submit" class="btn btn-success" value="Zapisz"></tr>
+	      		</div>
+	    	</div>
+	</div>
 </div>
 
 
