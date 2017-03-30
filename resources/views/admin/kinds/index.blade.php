9f142e0b33350cd2aab847a1c4bb03d273c6b8eb
Patryk Piwko <patrykpiwko123412@gmail.com>
2017-03-30 11:19:00 +0200

    refractor

--- resources/views/admin/kinds/index.blade.php
+++ resources/views/admin/kinds/index.blade.php
@@ -6,18 +6,18 @@
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
     	<div class="panel panel-info">
-			<div class="panel-heading">
-    @foreach($kinds as $kind)
-     <form method="post" action="/admin/edit_kind/{{$kind->id}}">
-    	{!! csrf_field() !!}
-      <p>Nazwa: <input style="width:150px" id="role_name" type="text" class="form-control" name="name" value="{{ $kind ->name }}">
-          <input type="submit" class="btn btn-success" value="Zapisz">
-     </form>
-          <a href="/admin/delete_kind/{{$kind->id}}" class="btn btn-danger">Usuń</a>
-     @endforeach
-     </form>
-     </div>
-     </div>
+		<div class="panel-heading">
+		    @foreach($kinds as $kind)
+			     <form method="post" action="/admin/edit_kind/{{$kind->id}}">
+				{!! csrf_field() !!}
+			      <p>Nazwa: <input style="width:150px" id="role_name" type="text" class="form-control" name="name" value="{{ $kind ->name }}">
+				  <input type="submit" class="btn btn-success" value="Zapisz">
+			     </form>
+				  <a href="/admin/delete_kind/{{$kind->id}}" class="btn btn-danger">Usuń</a>
+		     @endforeach
+     			</form>
+    		 </div>
+     	</div>
     </div>
   </div>
 </div>
@@ -25,18 +25,18 @@
 
 
 <div class="row">
-<div class ="col-md-3 col-md-offset-4">
-	<div class="panel panel-info">
-		<div class="panel-heading"> 
-			<h3 class="panel-title">Dodaj nowa kategorie</h3> </div>
-	      <form method="post" action="/admin/add_kind">
-	      {!! csrf_field() !!}
-	       	<p>Nazwa: <input style="width:150px" id="kind" type="text" class="form-control" name="name">
-			<br>
-			<input type="submit" class="btn btn-success" value="Zapisz"></tr>
-      </div>
-    </div>
-  </div>
+	<div class ="col-md-3 col-md-offset-4">
+		<div class="panel panel-info">
+			<div class="panel-heading"> 
+				<h3 class="panel-title">Dodaj nowa kategorie</h3> </div>
+				<form method="post" action="/admin/add_kind">
+					{!! csrf_field() !!}
+					<p>Nazwa: <input style="width:150px" id="kind" type="text" class="form-control" name="name"><br>
+					<input type="submit" class="btn btn-success" value="Zapisz"></tr>
+				</form>
+			</div>
+		</div>
+	 </div>
 </div>
 
 
