0233b9aa98cc67769f46d7ae8050f657942eff26
Patryk Piwko <patrykpiwko123412@gmail.com>
2017-03-30 11:25:24 +0200

    refractor

--- resources/views/admin/products/addProducts.blade.php
+++ resources/views/admin/products/addProducts.blade.php
@@ -1,57 +1,51 @@
 @extends('layouts.admin')
 @section('content')
 <div class="row">
-
    <div class="col-md-8 col-md-offset-3">
-      	
-      	
       	    <form enctype="multipart/form-data" method="POST" action="/admin/add_product/store">
 	      	{!! csrf_field() !!}
 	      	<div class="col-md-3">
-		      	Nazwa Produktu:
+		      		Nazwa Produktu:
 					<input style="width:150px" id="product" type="text" class="form-control" name="product">
 				Cena:
 					<input style="width:150px" id="price" type="text" class="form-control" name="price">
 				Ilość:
 					<input style="width:150px" id="amount" type="text" class="form-control" name="amount">
-			</div>
-			<div class="col-md-3">
-				Rodzaj:
-					<select name="kind">
+		</div>
+		<div class="col-md-3">
+			Rodzaj:
+				<select name="kind">
 					@foreach($kinds as $kind)
 					  <option value="{{$kind->name}}">{{$kind->name}}</option>
 					@endforeach
-					</select>
-					
-				Przeznaczenie:
-					<input style="width:150px" id="appropriaton" type="text" class="form-control" name="intended">
-				Wymiary Ogólne:
-					<input style="width:150px" id="general_size" type="text" class="form-control" name="general_size">
-				Wymiary:
-					<input style="width:150px" id="size" type="text" class="form-control" name="size">
-			</div>
-			<div class="col-md-3">
-				Promocja:
-					<input style="width:150px" id="promotion" type="text" class="form-control" name="promotion">
-				Procent promocji:
-					<input style="width:150px" id="percent_promotion" type="text" class="form-control" name="percent_promotion">
-				Tekst promocji:
-					<input style="width:150px" id="text_promotion" type="text" class="form-control" name="text_promotion">
-			
-				</div>
-						</div>
+				</select>
+			Przeznaczenie:
+				<input style="width:150px" id="appropriaton" type="text" class="form-control" name="intended">
+			Wymiary Ogólne:
+				<input style="width:150px" id="general_size" type="text" class="form-control" name="general_size">
+			Wymiary:
+				<input style="width:150px" id="size" type="text" class="form-control" name="size">
+		</div>
+		<div class="col-md-3">
+			Promocja:
+				<input style="width:150px" id="promotion" type="text" class="form-control" name="promotion">
+			Procent promocji:
+				<input style="width:150px" id="percent_promotion" type="text" class="form-control" name="percent_promotion">
+			Tekst promocji:
+				<input style="width:150px" id="text_promotion" type="text" class="form-control" name="text_promotion">
+
+		</div>
+	</div>
 </div>
 	{!! Form::file('image') !!}
-	
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
 			Opis:
 				<textarea class="desc" name="desc"></textarea>
 			<input type="submit" value="Dodaj">
-			</form>
-		</div>
-
+		</form>
 	</div>
+</div>
 
 
 @endsection
