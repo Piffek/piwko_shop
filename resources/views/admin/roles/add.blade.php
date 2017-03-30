1043b31116713774a82d67d3987faec91f49fa66
Patryk Piwko <patrykpiwko123412@gmail.com>
2017-03-30 11:22:47 +0200

    refractor

--- resources/views/admin/roles/add.blade.php
+++ resources/views/admin/roles/add.blade.php
@@ -27,12 +27,12 @@
 <div class="row">
 	<div class ="col-md-3 col-md-offset-4">
 		<div class="panel panel-info">
-			<div class="panel-heading"> 
-				<h3 class="panel-title">Dodaj nowa role</h3> </div>
-				      	<form method="post" action="/admin/add_role">
-				      	{!! csrf_field() !!}
+			<div class="panel-heading"><h3 class="panel-title">Dodaj nowa role</h3> </div>
+				<form method="post" action="/admin/add_role">
+					{!! csrf_field() !!}
 					<p>Nazwa: <input style="width:150px" id="role_name" type="text" class="form-control" name="name"><br>
 					<input type="submit" class="btn btn-success" value="Zapisz"></tr>
+				</form>
 	      		</div>
 	    	</div>
 	</div>
