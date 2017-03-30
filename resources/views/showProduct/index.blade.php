e9583c763a8c6f252aae48dee2f2d3ac0b7224a1
Patryk Piwko <patrykpiwko123412@gmail.com>
2017-03-30 10:56:50 +0200

    refractor

--- resources/views/showProduct/index.blade.php
+++ resources/views/showProduct/index.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.app')
 
-
-
-
-
 @section('content')
 <div class="row">
 	<div class="col-md-6 col-md-offset-3">
@@ -13,8 +9,8 @@
 					<h1>{{$items->product}}</h1>
 					<h2>{{$items->price}} zł</h2>
 					<h3>{{$items->desc}}</h3>
-@if(Auth::check())
-	@if($items->amount > 0)
+				@if(Auth::check())
+					@if($items->amount > 0)
 					<form method="post" action="/koszyk/store">
 						{!! csrf_field() !!}
 						<input hidden name="product" value="{{$items->product}}"></td></tr>
@@ -24,40 +20,41 @@
 						<input for="amount" maxlength="3" type="text" name="amount" required></td></tr>
 						<input type="submit" value="Do koszyka!"><br></tr>
 						<div id="galeria"></div>	
-	@else
-		Tego produktu aktualnie nie ma w magazynie
-	@endif
-@else
-	@if($items->amount > 0)
+					</form>
+					@else
+						Tego produktu aktualnie nie ma w magazynie
+					@endif
+				@else
+					@if($items->amount > 0)
 					Ilosc:
 					<form method="post" action="/koszyk_goscia/{{$items->id}}">
 					{!! csrf_field() !!}
 					<input type="hidden" name="random_id_product" value='<?php echo rand(0, 1000000); ?>'>
 					<input for="amount" maxlength="3" type="text" name="amount" required></td></tr>
 					<input type="submit" value="Do koszyka!"><br></tr>
-	@else
-		Tego produktu aktualnie nie ma w magazynie
-	@endif
-@endif
-						<div id="galeria"></div>	
-							<?php 
-							$zdjecie=$items->id;
-							$katalog = "pokaz_produkt/miniaturki/$zdjecie";
-							$katalogminiaturki = "pokaz_produkt/miniaturki/$zdjecie";
-							$galeria = opendir( $katalog );
-							while ( $zdjecie = readdir( $galeria ) )
-							{
-									
-								$odczyt = pathinfo( $katalog.'/'.$zdjecie );
-								if ( $odczyt['extension']  == 'jpg' )
-								{
-							
-									echo '<a href="/'.$katalog.'/'.$zdjecie.'" class="highslide" onclick="return hs.expand(this)" title="Zdjęcie: '.$zdjecie.'"><img width="200" height="133" src="/'.$katalogminiaturki.'/'.$zdjecie.'" alt="Zdjęcie: '.$zdjecie.'" /></a>';
-								}
-							
-							}
-							closedir($galeria);
-						?>	
+					@else
+						Tego produktu aktualnie nie ma w magazynie
+					@endif
+				@endif
+				<div id="galeria"></div>	
+					<?php 
+					$zdjecie=$items->id;
+					$katalog = "pokaz_produkt/miniaturki/$zdjecie";
+					$katalogminiaturki = "pokaz_produkt/miniaturki/$zdjecie";
+					$galeria = opendir( $katalog );
+					while ( $zdjecie = readdir( $galeria ) )
+					{
+
+						$odczyt = pathinfo( $katalog.'/'.$zdjecie );
+						if ( $odczyt['extension']  == 'jpg' )
+						{
+
+							echo '<a href="/'.$katalog.'/'.$zdjecie.'" class="highslide" onclick="return hs.expand(this)" title="Zdjęcie: '.$zdjecie.'"><img width="200" height="133" src="/'.$katalogminiaturki.'/'.$zdjecie.'" alt="Zdjęcie: '.$zdjecie.'" /></a>';
+						}
+
+					}
+					closedir($galeria);
+				?>	
 				</div>
 			</div>
 		</div>
