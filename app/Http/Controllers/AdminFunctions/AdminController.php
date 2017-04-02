7079d1b8a41d06a16db8dc15654ef3e121aa45a0
Piffek <patrykpiwko123412@gmail.com>
2017-04-02 19:57:14 +0200

    delete unnecessary code

--- app/Http/Controllers/AdminFunctions/AdminController.php
+++ app/Http/Controllers/AdminFunctions/AdminController.php
@@ -21,13 +21,10 @@ class AdminController extends Controller
 	
 	public function productChart()
 	{
-		//DB::table('booking')->selectRaw('count(id) as bookings_count, dayofweek(created_at) as weekday')->groupBy('weekday')->get();
-		//return view('admin.chart.productsChart');
-	$productChart = DB::table('buyings')->selectRaw('count(id) as buyings_count, product as products')->groupBy('products')->get();
-	$product = $productChart->keyBy('products');
-	$productsWithDB = Items::all();
-	return view('admin.chart.productsChart',compact('product', 'productsWithDB'));
-
+		$productChart = DB::table('buyings')->selectRaw('count(id) as buyings_count, product as products')->groupBy('products')->get();
+		$product = $productChart->keyBy('products');
+		$productsWithDB = Items::all();
+		return view('admin.chart.productsChart',compact('product', 'productsWithDB'));
 	}
 	
 	public function soldChart()
@@ -35,20 +32,12 @@ class AdminController extends Controller
 		$soldChart = DB::table('buyings')->selectRaw('count(id) as buyings_count, dayofweek(created_at) as weekday')->groupBy('weekday')->get();
 		$sold = $soldChart->keyBy('weekday');
 		return view('admin.chart.soldChart',compact('sold'));
-		
 	}
 	
 	public function deliveryChart()
 	{
 		$deliveryChart = new ChartAdmin('buyings', 'delivery', 'UPS', 'DPD','delivery2','admin.chart.deliveryChart');
 		return $deliveryChart->chartDelivery();
-
-		/*$delivery2 = DB::table('buyings')->selectRaw('count(id) as buyings_count, delivery as deli')->groupBy('deli')->get();
-		$dayUPS = DB::table('buyings')->selectRaw('count(id) as buyings_count, dayofweek(created_at) as weekday')->where('delivery','UPS')->groupBy('weekday')->get();
-		$dayDPD = DB::table('buyings')->selectRaw('count(id) as buyings_count, dayofweek(created_at) as weekday')->where('delivery', 'DPD')->groupBy('weekday')->get();
-		$UPS=$dayUPS->keyBy('weekday');
-		$DPD=$dayDPD->keyBy('weekday');*/
-		//return View::make('admin.chart.deliveryChart',compact('UPS','DPD','delivery2'));
 	}
 	
 	public function paidChart()
