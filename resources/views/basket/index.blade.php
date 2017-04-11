48a3df9d8212d1d912d9641bf229d3ab6918e61f
Piffek <patrykpiwko123412@gmail.com>
2017-04-11 21:22:26 +0200

    fix front code - basket

--- resources/views/basket/index.blade.php
+++ resources/views/basket/index.blade.php
@@ -35,7 +35,7 @@
 						<td>{{$baskets->amount}}</td>
 						<td><a class="btn btn-primary" data-toggle="modal" data-target="#changeAmount{{$baskets->id }}">Zmień ilość </a></td>
 						<td>
-							<a href="{{route('basketDestroy',['id'=>$baskets->id]) }}">Usuń</a>
+							<a class="btn btn-danger pull-right" href="{{route('basketDestroy',['id'=>$baskets->id]) }}">Usuń</a>
 						</td>
 					</tbody>
 					<div class="modal fade bs-example-modal-sm" id="changeAmount{{$baskets->id}}" tabindex="-1" role="dialog" >
@@ -62,46 +62,45 @@
 				</table>
 					</div>	
 			<a class='btn btn-success' href="{{route('transaction')}}">Zrealizuj</a>
-			</form>	
 		@endif
 		
 	</div>
 @else
-@if(!Session::has('basket'))
-<div class="col-md-8 col-md-offset-2">
-	<div class="panel panel-default">
-	  <div class="panel-heading">Koszyk</div>
-		<table class="table">
-		  Nie masz produktów w koszyku
-		 </table>
-	</div>
-</div>
-@else
-<div class="col-md-8 col-md-offset-2">
-	<div class="panel panel-default">
-		<div class="panel-heading">Koszyk</div>
+	@if(!Session::has('basket'))
+	<div class="col-md-8 col-md-offset-2">
+		<div class="panel panel-default">
+		  <div class="panel-heading">Koszyk</div>
 			<table class="table">
-			  	<thread>
-			  		<tr>
-						<th>Produkt</th>
-						<th>Cena</th>
-						<th>Ilosc</th>
-			  		</tr>
-			  	</thread>
-				 @foreach($products as $p)
-					<tbody>
-					   	<td>{{$p['product']}}</td>
-						<td>{{$p['price']*$p['amount']}}</td>
-						<td>{{$p['amount']}}</td>
-						<td><a href="{{route('deleteBasketGuest',['id'=>$p['random_id']])}}" class="btn btn-danger pull-right" role="button">Usuń</a></td>
-					</tbody>
-				@endforeach		
-			</table>
+			  Nie masz produktów w koszyku
+			 </table>
+		</div>
+	</div>
+	@else
+	<div class="col-md-8 col-md-offset-2">
+		<div class="panel panel-default">
+			<div class="panel-heading">Koszyk</div>
+				<table class="table">
+				  	<thread>
+				  		<tr>
+							<th>Produkt</th>
+							<th>Cena</th>
+							<th>Ilosc</th>
+				  		</tr>
+				  	</thread>
+					 @foreach($products as $p)
+						<tbody>
+						   	<td>{{$p['product']}}</td>
+							<td>{{$p['price']*$p['amount']}}</td>
+							<td>{{$p['amount']}}</td>
+							<td><a href="{{route('deleteBasketGuest',['id'=>$p['random_id']])}}" class="btn btn-danger pull-right" role="button">Usuń</a></td>
+						</tbody>
+					@endforeach		
+				</table>
+			</div>	
+			<a class="btn btn-success" href="{{route('guestTransaction')}}">Zrealizuj</a>
+			
 		</div>	
-		<a class="btn btn-success" href="{{route('guestTransaction')}}">Zrealizuj</a>
-		
-	</div>	
-@endif
+	@endif
 @endif
 </div>
 
