90a5da1fa0041ff0b30e57c24ad5d23a75dbd075
Patryk Piwko <patrykpiwko123412@gmail.com>
2017-05-16 10:23:30 +0200

    ref

--- app/Http/Controllers/AdminFunctions/AdminController.php
+++ app/Http/Controllers/AdminFunctions/AdminController.php
@@ -14,34 +14,29 @@ use App\Buyings;
 class AdminController extends Controller
 {
 	
-	public function index()
-	{
+	public function index(){
 		return view('admin.index');
 	}
 	
-	public function productChart()
-	{
+	public function productChart(){
 		$productChart = DB::table('buyings')->selectRaw('count(id) as buyings_count, product as products')->groupBy('products')->get();
 		$product = $productChart->keyBy('products');
 		$productsWithDB = Items::all();
 		return view('admin.chart.productsChart',compact('product', 'productsWithDB'));
 	}
 	
-	public function soldChart()
-	{
+	public function soldChart(){
 		$soldChart = DB::table('buyings')->selectRaw('count(id) as buyings_count, dayofweek(created_at) as weekday')->groupBy('weekday')->get();
 		$sold = $soldChart->keyBy('weekday');
 		return view('admin.chart.soldChart',compact('sold'));
 	}
 	
-	public function deliveryChart()
-	{
+	public function deliveryChart(){
 		$deliveryChart = new ChartAdmin('buyings', 'delivery', 'UPS', 'DPD','delivery2','admin.chart.deliveryChart');
 		return $deliveryChart->chartDelivery();
 	}
 	
-	public function paidChart()
-	{
+	public function paidChart(){
 		$deliveryChart = new ChartAdmin('buyings', 'paying', 'przedplata', 'pobranie','Calosc','admin.chart.paidChart');
 		return $deliveryChart->chartDelivery();
 	}
