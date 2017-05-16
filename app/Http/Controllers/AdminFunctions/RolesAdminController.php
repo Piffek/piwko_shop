0b3635f392870be83f899156ad6a898718244d8c
Patryk Piwko <patrykpiwko123412@gmail.com>
2017-05-16 10:31:47 +0200

    Update RolesAdminController.php

--- app/Http/Controllers/AdminFunctions/RolesAdminController.php
+++ app/Http/Controllers/AdminFunctions/RolesAdminController.php
@@ -10,65 +10,15 @@ use Session;
 
 class RolesAdminController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
+    public function index(){
     	$roles = Roles::all();
         return view('admin.roles.add',compact('roles'));
     }
     
-    public function addRole(Request $request)
-    {
+    public function addRole(Request $request){
     	Roles::create($request->all());
     	Session::flash('success','Dodano nową role');
     	return redirect()->back();
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        
     }
 
     /**
@@ -78,8 +28,7 @@ class RolesAdminController extends Controller
      * @param  Roles  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Roles $id)
-    {
+    public function update(Request $request, Roles $id){
         $id->update($request->all());
         Session::flash('success', 'Pomyślnie edytowano role');
         return redirect()->back();
@@ -91,8 +40,7 @@ class RolesAdminController extends Controller
      * @param  Roles  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Roles $id)
-    {
+    public function destroy(Roles $id){
         $id->delete();
         Session::flash('success', 'Usunięto role');
         return redirect()->back();
