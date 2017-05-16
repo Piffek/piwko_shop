a4aaeecfecde77260ae1e305078429f74094b540
Patryk Piwko <patrykpiwko123412@gmail.com>
2017-05-16 11:01:25 +0200

    Update MailingLogon.php

--- app/Jobs/MailingLogon.php
+++ app/Jobs/MailingLogon.php
@@ -22,59 +22,47 @@ class MailingLogon implements ShouldQueue
 	protected $dbName;
 	protected $whichDB;
 	protected $userEmail;
-    /**
-     * Create a new job instance.
-     *
-     * @return void
-     */
-    public function __construct($id,$dbName,$whichDB)
-    {
+   
+    public function __construct($id,$dbName,$whichDB){
     	$this->id = $id;
     	$this->dbName = $dbName;
     	$this->whichDB = $whichDB;
     }
 
-    /**
-     * Execute the job.
-     *
-     * @return void
-     */
-    public function handle()
-    {
+ 
+    public function handle(){
     	$admin_data =  DB::table('roles_has_users')
     	->join('users', 'roles_has_users.users_id', '=', 'users.id')
     	->select('users.*')
     	->where([
-    			['roles_id','=','1'],
+		['roles_id','=','1'],
     	])->first();
     	 
-
+	//check data customer who get product
     	$customers = DB::table($this->whichDB)->where($this->dbName,$this->id)->get();
-    	
-    	foreach ($customers as $customer)
-    	{
+ 
+    	foreach ($customers as $customer){
     		Mail::to('patrykpiwko123412@gmail.com')->send(new MailToOwner($customer->product, $customer->amount, isset($customer->lastnameonaccount) ? $customer->lastnameonaccount : $customer->surname));
-    		$customers_id = DB::table($this->whichDB)->where('id',$customer->id)->get();
+    		
+		//check data customer and insert to bill
+		$customers_id = DB::table($this->whichDB)->where('id',$customer->id)->get();
     	
     		$pdf = \PDF::loadView('emails.bills_logout', array('customers_id' => $customers_id, 'admin_data' =>$admin_data));
-    		\Mail::send('emails.thanks_for_buyings', array('customers_id' => $customers_id,'admin_data' =>$admin_data), function($message) use($pdf,$customer)
-    		{
-    			if(empty($customer->email))
-    			{
-    				$emails = User::where('id', $customer->id_user)->get();
-    				foreach($emails as $email)
-    				{
-    					$userEmail  = $email->email;
-    				}
-    			}else
-    			{
-    				$userEmail = $customer->email;
-    			}
+		
+		//if customer has account get e-mail with table User and set to variable userEmail
+		//else set to variable userEmail email with $customer->email
+		if(empty($customer->email)){
+			$emails = User::where('id', $customer->id_user)->get();
+			foreach($emails as $email){
+				$userEmail  = $email->email;
+			}
+		}else{
+			$userEmail = $customer->email;
+		}
+		$message->from('patrykpiwko123412@com', 'John Smith')->subject('Zakup'.$customer->id.'');
+		$message->to($userEmail, 'John Smith')->subject('Zakup'.$customer->id.'');
 
-    			$message->from('patrykpiwko123412@com', 'John Smith')->subject('Zakup'.$customer->id.'');
-    			$message->to($userEmail, 'John Smith')->subject('Zakup'.$customer->id.'');
-    			
-    			$message->attachData($pdf->output(), 'faktura_nr_'.$customer->id.'.pdf');
+		$message->attachData($pdf->output(), 'faktura_nr_'.$customer->id.'.pdf');
     		});
     	}
     	
