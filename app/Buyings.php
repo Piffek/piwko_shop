3a5514893ba2ae99d4629b8aee7d69efc5b8054d
Patryk Piwko <patrykpiwko123412@gmail.com>
2017-05-16 10:46:14 +0200

    Update Buyings.php

--- app/Buyings.php
+++ app/Buyings.php
@@ -8,17 +8,18 @@ class Buyings extends Model
 
 	
 	protected $fillable = [
-			'id','id_product','product', 'price', 'amount', 'id_user','surname','street','city','nip','companyname' ,'delivery','paying','id_adress_delivery', 'state'
-			];
+	    'id','id_product','product', 'price', 'amount', 'id_user','surname','street','city','nip',
+	    'companyname' ,'delivery','paying','id_adress_delivery', 'state',
+	];
 	
 	protected $hidden = [
-			'remember_token',
+	    'remember_token',
 	];
 	
 	protected $table = 'buyings';
-
-	public function getId()
-	{
+	
+	//use to add id role to table UserHasRole during first login after registration.
+	public function getId(){
 		return $this->id;
 	}
 
