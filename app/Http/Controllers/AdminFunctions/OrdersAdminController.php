872f1fcedfb4156c9979b2cd5653020be0025c09
Patryk Piwko <patrykpiwko123412@gmail.com>
2017-05-16 10:27:26 +0200

    Update OrdersAdminController.php

--- app/Http/Controllers/AdminFunctions/OrdersAdminController.php
+++ app/Http/Controllers/AdminFunctions/OrdersAdminController.php
@@ -12,17 +12,16 @@ class OrdersAdminController extends Controller
 {
 	
 	
-	public function index($user_id)
-	{
+	public function index($user_id){
 		$orders_this_customers = Buyings::where('id_user',$user_id)->get();
 		return view('admin.orders.orders_this_customers',compact('orders_this_customers'));
 	}
 	
-	public function allOrders()
-	{
+	public function allOrders(){
 		$all_orders = Buyings::orderBy('id', 'desc')->get();
 		return view('admin.orders.allOrders',compact('all_orders'));
 	}
+	
 	
 	/**
 	 * Update the specified resource in storage.
@@ -31,8 +30,7 @@ class OrdersAdminController extends Controller
 	 * @param  Buyings  $id
 	 * @return \Illuminate\Http\Response
 	 */
-	public function thisOrder(Buyings $id)
-	{
+	public function thisOrder(Buyings $id){
 		$this_order = clone $id;
 		return view('admin.orders.thisOrder',compact('this_order'));
 	}
@@ -44,8 +42,7 @@ class OrdersAdminController extends Controller
 	 * @param  Buyings  $id
 	 * @return \Illuminate\Http\Response
 	 */
-	public function deleteOrder(Buyings $id)
-	{
+	public function deleteOrder(Buyings $id){
 		$id->delete();
 		Session::flash('success','Usunięto zamówienie.');
 		return redirect()->action('AdminFunctions\OrdersAdminController@allOrders');
