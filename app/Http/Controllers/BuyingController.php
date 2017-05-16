1561d83b2f23f942af9b2b7eb817d56e0804af95
Patryk Piwko <patrykpiwko123412@gmail.com>
2017-05-16 10:36:02 +0200

    Update BuyingController.php

--- app/Http/Controllers/BuyingController.php
+++ app/Http/Controllers/BuyingController.php
@@ -11,32 +11,19 @@ use Session;
 
 class BuyingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request)
-    {
-    	$buying = Buyings::where('id_user',Auth::user()->id)->paginate('10');
-    	return view('buying',compact('buying'));
+
+    public function index(Request $request){
+    	$buying = Buyings::where('id_user', Auth::user()->id)->paginate('10');
+    	return view('buying', compact('buying'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create(Request $request, StoreCashier $cashier)
-    {
-
+    public function create(Request $request, StoreCashier $cashier){
     	$this->validate($request, [
     		'city' => 'required_if:id_adress_delivery,0',
     		'street' => 'required_if:id_adress_delivery,0',
     	]);
     	
-    	if(empty($request -> id_adress_delivery))
-    	{
+    	if(empty($request -> id_adress_delivery)){
     		$city = Auth::user()->city;
     		$street = Auth::user()->street;
     		$id_adress_delivery = '0';
@@ -47,8 +34,7 @@ class BuyingController extends Controller
     				'delivery_method'=>$request->delivery,
     				'payment_method'=>$request->paying,
     		);
-    	}else if(!empty($request -> id_adress_delivery))
-    	{
+    	}else if(!empty($request -> id_adress_delivery)){
     		$city = $request -> city;
     		$street = $request -> street;
     		$id_adress_delivery = $request -> id_adress_delivery;
@@ -62,67 +48,9 @@ class BuyingController extends Controller
     	}
     		
     	 $cashier->checkout($adress_delivery);
-    	
-    	
     	Session::flash('success','Zakupiono produkt.');
+        
     	return redirect()->action('BuyingController@index');
-
-    }
-    
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-    	
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy()
-    {
 
     }
 }
