ba6fd5d0ff5cd0933c9f865b38c4c5a240b75bd8
Piffek <patrykpiwko123412@gmail.com>
2017-06-07 19:48:45 +0200

    add view provider

--- app/Http/Controllers/AdminFunctions/ToDoListController.php
+++ app/Http/Controllers/AdminFunctions/ToDoListController.php
@@ -5,18 +5,10 @@ namespace App\Http\Controllers\AdminFunctions;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\ToDoList;
-use App\User;
 
 
 class ToDoListController extends Controller
 {  
-    public function index(){
-    	//$todolist = ToDoList::all();
-	    //$todolist = User::find()->toDoList()->where('id_user', Auth::user()->id)->get;
-        return $todolist;
-    }
-    
-
     public function addToDoList(Request $request){
 		ToDoList::create($request->all());
     }
