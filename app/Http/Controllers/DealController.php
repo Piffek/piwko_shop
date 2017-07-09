58e541989480c380511ce5d591205af2122fcba0
Piffek <patrykpiwko123412@gmail.com>
2017-07-09 18:28:27 +0200

    refactoring DealController

--- app/Http/Controllers/DealController.php
+++ app/Http/Controllers/DealController.php
@@ -4,18 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Baskets;
-use View;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Auth;
-use App\User;
-use App\AdressDelivery;
-use DB;
+use App\Repositories\UserRepository as User;
+use App\Repositories\AdressDeliveryRepository as AdressDelivery;
+use App\Repositories\BasketsRepository as Baskers;
 
 class DealController extends Controller
 {
+    
+    public function __construct(User $user, AdressDelivery $adressDelivery, Baskers $basket){
+        $this->user = $user;
+        $this->adressDelivery = $adressDelivery;
+        $this->basket = $basket;
+    }
+    
+    /**
+     * Show form.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index(){	
-    	$data_users = User::whereid(Auth::user()->id)->get();
-    	$data_deals = AdressDelivery::whereid_user(Auth::user()->id)->get();
-    	$baskets = Baskets::where('id_user', Auth::user()->id)->get();
+        $data_users = $this->user->where('id', Auth::user()->id)->get();
+        $data_deals = $this->adressDelivery->where('id_user', Auth::user()->id)->get();
+        $baskets = $this->basket->where('id_user', Auth::user()->id)->get();
  
     	return View::make('transaction')
     	->with(compact('data_users'))
